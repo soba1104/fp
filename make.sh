802fb440e60f288ed2b07406fc052f5098d4ad04
soba1104 <shiba@rvm.jp>
2015-04-24 14:21:24 +0900

    ビルドスクリプトの更新

--- make.sh
+++ make.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-cd src/ss && cmake . && make clean && make && make install
+cd src/ss
+cmake -D DEBUG_BUILD=${1} . && make clean && make VERBOSE=true && make install
 cd ../../
+
 make clean
-cmake .
-make
+cmake -D DEBUG_BUILD=${1} .
+make VERBOSE=true
