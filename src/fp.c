8ebc759e4fcccad5a7da3e324e4e35f11344332d
soba1104 <shiba@rvm.jp>
2015-07-06 23:50:17 +0900

    error log で typo している箇所があったのを修正

--- src/fp.c
+++ src/fp.c
@@ -825,7 +825,7 @@ static bool session_process_bufsize(fp_session *session) {
     char *newbuf = NULL;
 
     if (!readn(session, &newbufsize, sizeof(newbufsize))) {
-        ss_err(logger, "failed to read new buffer sizen");
+        ss_err(logger, "failed to read new buffer size");
         goto err;
     }
     newbufsize = ntohll(newbufsize);
